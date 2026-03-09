869dadf347891690e4dc4ba43276eabf30d6c1d7
Xillioneur <241650870+Xillioneur@users.noreply.github.com>
2026-03-09 15:07:47 -0400

    Prepare to deploy to Vercel.

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,14 +11,9 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: "#1e3a8a", // Catholic Blue
     icons: [
       {
-        src: "/icons/icon-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        src: "/icons/icon-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
+        src: "/favicon.ico",
+        sizes: "any",
+        type: "image/x-icon",
       },
     ],
   };
